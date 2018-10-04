422c7e12052bdcbc6774e464a78193a50d7d2d64
samutamm <samutamm@gmail.com>
2018-10-04 12:29:22 +0200

    normalized

--- IRModel.py
+++ IRModel.py
@@ -5,7 +5,8 @@ class IRModel:
     
     def __init__(self, weighter):
         self.weighter = weighter
-        self.norm_file = "indexes/document_norm_"
+        self.norm_file = "indexes/document_norm_" # TODO move to weighter
+        self.docNorms = self.getDocNorms() 
     
     def getScores(self, query):
         pass
@@ -17,15 +18,40 @@ class Vectoriel(IRModel):
     
     def getScores(self, query, normalized=False):
         wtq = self.weighter.getWeightsForQuery(query)    
-        s = 0
+        s = {}
+        
         for term in query.keys():
-            s += (wtq[term] * np.array(self.weighter.getDocWeightsForStem(term).values())).sum()
+            term_docs = self.weighter.getDocWeightsForStem(term)
+            
+            term_norm = np.sqrt((np.array(term_docs.values()) ** 2).sum())
+            for doc_id in term_docs.keys():
+                doc_norm = self.docNorms[str(doc_id)]
+                if doc_id not in s:
+                    s[doc_id] = 0
+                norm = term_norm * doc_norm if normalized else 1
+                s[int(doc_id)] += wtq[term] * term_docs[doc_id] / norm
+        
         return s
     
-    def getRanking(self, query):
-        #for d in doc:
-        pass
-    
+    def getRanking(self, query, normalized=False):
+        stemmer = self.weighter.stemmer
+        query_vector = stemmer.getTextRepresentation(query)
+        scores = self.getScores(query_vector, normalized=normalized)
+        
+        scores_unranked = np.array([(str(key),float(scores[key])) for key in scores.keys()], 
+                                   dtype=(np.character,np.float))
+        scores_ranked = scores_unranked[scores_unranked[:,1].argsort()[::-1]].tolist()
+        
+        # let's add all documents that does not contain query words
+        all_doc_ids = self.weighter.index.getDocIds()
+        for doc_id in all_doc_ids:
+            if doc_id not in scores:
+                scores_ranked.append([str(doc_id), 0])
+                
+        rank = scores_ranked
+        return [[str(rank[i][0]), float(rank[i][1])] for i in range(len(rank))]
+            
+    # TODO move to weighter! ca depend du weighter
     def calculeNorms(self):
         class_name = self.__class__.__name__.lower()
         norm_filename = self.norm_file + class_name
@@ -50,13 +76,9 @@ class Vectoriel(IRModel):
         with open(norm_filename, 'wb') as f:
             cPickle.dump(norms, f)
             
-    def getNormDocs(self, docs):
+    def getDocNorms(self):
         class_name = self.__class__.__name__.lower()
         norm_filename = self.norm_file + class_name
         with open(norm_filename, 'rb') as f:
-            norms = cPickle.Unpickler(f).load()
-        #import pdb; pdb.set_trace()
-        result = []
-        for d in docs:
-            result.append(norms[d])
-        return result
+            return cPickle.Unpickler(f).load()
+        #return norms[str(doc_id)]

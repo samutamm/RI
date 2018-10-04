eb196829b0ed7d4d56c20f96ccae01fa3f1d3837
P4E <p4e@posteo.de>
2018-10-04 13:29:37 +0200

    corrections QueryParser

--- ParserQuery.py
+++ ParserQuery.py
@@ -1,32 +1,30 @@
 from ParserCACM import ParserCACM 
+from TextRepresenter import PorterStemmer
 
 class QueryParser():
   def initFile(self, filename_queries, filename_jugements):
-    self.dict_queries = {}
+    self.queries_ = {}
     parser = ParserCACM()
     parser.initFile(filename_queries)
-    while True:
+    doc = parser.nextDocument()
+    while doc:
+      self.queries_[int(doc.getId())] = Query(doc.getId(), doc.getText().replace('\n', ''))
       doc = parser.nextDocument()
-      if doc is None:
-        break
-      print(doc.getId())
-      self.dict_queries[doc.getId()] = Query(doc.getId(), doc.getText())
-    return self.dict_queries
-    '''with open(filename_jugements, 'rb') as f:
+    with open(filename_jugements, 'rb') as f:
       for line in f:
         line = line.split(' ') 
-        self.dict_queries[line[0]].add_relevant(line[1])
-    self.index_ = 0'''
+        self.queries_[int(line[0])].add_relevant(line[1])
+    self.index_ = 0
   def nextQuery():
     self.index_ += 1
-    return self.dict_queries[self.index-1]
+    return self.queries_[self.index-1]
   
 class Query:
   def __init__(self, id_, text_, relevants=[]):
     self.id_ = id_
     self.text_ = text_
     self.relevants_ = relevants
-  def add_relevant(relevant):
+  def add_relevant(self, relevant):
     if type(relevant) == list:
       self.relevants_.extend(relevant)
     else:

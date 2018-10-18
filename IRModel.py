bfb987520298a007949fd9363b683374079bf188
Pierre <p4e@posteo.de>
2018-10-18 12:07:45 +0200

    fin modèle langue, correction count_ranking, optimisation BM25

--- IRModel.py
+++ IRModel.py
@@ -13,16 +13,17 @@ class IRModel:
     def getRanking(self, query):
         pass
     
-    def _count_ranking(self, scores):
-        scores_unranked = np.array([(str(key),float(scores[key])) for key in scores.keys()], 
-                                   dtype=(np.character,np.float))
+    def _count_ranking(self, scores, minimum=0):
+        #scores_unranked = np.array([(str(key),float(scores[key])) for key in scores.keys()], 
+        #                           dtype=(np.character,np.float))
+
+        scores_unranked = np.array([(key, scores[key]) for key in scores.keys()])
         scores_ranked = scores_unranked[scores_unranked[:,1].argsort()[::-1]].tolist()
-        
         # let's add all documents that does not contain query words
         all_doc_ids = self.weighter.index.getDocIds()
         for doc_id in all_doc_ids:
             if int(doc_id) not in scores.keys():
-                scores_ranked.append([str(doc_id), 0])
+                scores_ranked.append([str(doc_id), minimum])
                 
         rank = scores_ranked
         return [[str(rank[i][0]), float(rank[i][1])] for i in range(len(rank))]
@@ -71,19 +72,27 @@ class Vectoriel(IRModel):
 class LanguageModel(IRModel):
     def __init__(self, weighter):
         super().__init__(weighter)
-        self.l_docs_ = {idx:sum(self.weighter.getDocWeightsForDoc(idx).values()) for idx in self.weighter.index.keys()}    
+        self.l_docs_ = {int(idx):sum(self.weighter.getDocWeightsForDoc(idx).values()) for idx in self.weighter.index.getDocIds()}    
     def getScores(self, query, lambd=1):
-        s = {} 
+        scores = {} 
         tw4q = self.weighter.getWeightsForQuery(query)
-        for t in query:
-            dw4t = self.weighter.getDocWeightsForStem()
-            tf_t_c = sum(dw4t.values())
-            l_c = sum(self.l_docs_.values())
-            for d in dw4t:
-                s['d'] += tw4q[t] * np.log( lambd*(dw4t[d]/self.l_docs_[d]) + (1-lambd)*(tf_t_c/l_c) )
-        return s 
-    def getRanking(self, query):
-        pass
+        l_c = sum(self.l_docs_.values())
+        for stem in query.keys():
+            dw4s = self.weighter.getDocWeightsForStem(stem)
+            tf_t_c = sum(dw4s.values())
+            print("Modèle sur corpus :", tf_t_c/l_c)
+            for d in dw4s.keys():
+                #print(dw4s[d]/self.l_docs_[d])
+                if d not in scores:
+                    scores[d] = 0
+                scores[d] += tw4q[stem] * np.log( lambd*(dw4s[d]/self.l_docs_[d]) + (1-lambd)*(tf_t_c/l_c) )
+        return scores
+    def getRanking(self, query, lambd=1):
+        stemmer = self.weighter.stemmer
+        query_vector = stemmer.getTextRepresentation(query)
+        scores = self.getScores(query_vector, lambd=lambd)
+        ranking = self._count_ranking(scores, minimum=-999)
+        return ranking
 
 
 # BM25 vectoriel
@@ -98,7 +107,8 @@ class BM25Model(IRModel):
     def __init__(self, weighter):
         super().__init__(weighter)
         self.N = len(weighter.index.getDocIds())
-        
+        self.L = {int(idx):sum(self.weighter.getDocWeightsForDoc(idx).values()) for idx in self.weighter.index.getDocIds()}    
+
     def getScores(self, query, k1=1, b=0.75):
         L_mean = self.weighter.doc_mean_length
         scores = {}
@@ -107,10 +117,10 @@ class BM25Model(IRModel):
             dw41 = self.weighter.getDocWeightsForStem(stem)
             idf = idf_prime(stem, dw41, self.N)
             for doc_id in dw41.keys():
-                L = len(self.weighter.index.getTfsForDoc(str(doc_id)))# doc lengths : optimisation --> precalcul
+                #L = len(self.weighter.index.getTfsForDoc(str(doc_id)))# doc lengths : optimisation --> precalcul
                 tf_td = dw41[doc_id]
                 nominator = (k1 + 1) * tf_td
-                denominator = k1 * ((1 - b) + b*L / L_mean) + tf_td
+                denominator = k1 * ((1 - b) + b*self.L[doc_id] / L_mean) + tf_td
                 if doc_id not in scores:
                     scores[doc_id] = 0
                 scores[doc_id] += idf * (nominator / denominator)

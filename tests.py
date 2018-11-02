05e56748336c9baad9c67557922fc2aebf69c424
P4E <p4e@posteo.de>
2018-11-02 14:41:08 +0100

    refactor test_pagerank, ajout test LinearMetaModel

--- tests.py
+++ tests.py
@@ -2,25 +2,25 @@ import pickle
 import numpy as np
 import pandas as pd
 
-from Document import Document
-from ParserCACM import ParserCACM
-from ParserQuery import QueryParser
-from porter import stem
-from TextRepresenter import PorterStemmer
-from EvalIRModel import EvalIRModel
+#from Document import Document
+#from ParserCACM import ParserCACM
+#from ParserQuery import QueryParser
+#from porter import stem
+#from TextRepresenter import PorterStemmer
+#from EvalIRModel import EvalIRModel
 from Index import Index, InvertedIndexPlaces
 from Weighter import WeighterBoolean, WeighterVector
-from IRModel import Vectoriel, LanguageModel, BM25Model
+from IRModel import Vectoriel, LanguageModel, BM25Model, LinearMetaModel
 from RandomWalk import *
 
 index = Index("cacm", "cacm/cacm.txt")
-#index.indexation()
 weighter = WeighterVector(index)
 #weighter.calculeNorms()
-#model = Vectoriel(weighter)
-bm25_model = BM25Model(weighter)
+#vectoriel_model = Vectoriel(weighter)
+#bm25_model = BM25Model(weighter)
 #language_model = LanguageModel(weighter)
-
+''' PageRank '''
+'''
 #EvalIRModel().evalModel(LanguageModel)
 #language_model.getRanking('computer sciences', lambd=1)
 ranking = bm25_model.getRanking('computer sciences')
@@ -35,8 +35,10 @@ pr_ranking = np.concatenate((Vq.reshape(-1,1), mus.reshape(-1,1)), axis=1)
 pr_ranking = pr_ranking[pr_ranking[:, 1].argsort(-1)[::-1]]
 
 print("BM25 ranking:", ranking)
-
 print("PR ranking:", pr_ranking)
+'''
 
-
+''' LinearMetaModel '''
+fl = Featurer(index)
+lmm = LinearMetaModel(featurers_list)
 

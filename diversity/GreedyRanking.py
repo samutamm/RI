7bc50bd5038a448485ec8b563f5ed1b28b979d60
samutamm <samutamm@gmail.com>
2018-12-18 21:11:30 +0100

    optimize and evaluate greedy algorithm

--- diversity/GreedyRanking.py
+++ diversity/GreedyRanking.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+import pandas as pd
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -12,30 +14,33 @@ class GreedyRanking:
         self.sim2 = cosine_similarity
         self.value_function = make_mmr(self.sim1)
 
-        self.vector_cache = {} # add vectors to cache to avoid recalculating them
+        self.all_doc_ids = list(index.getDocIds())
+        self.doc_vectors, self.identity2index, self.vectorizer = calculate_vector_presentation(self.index, self.all_doc_ids)
 
-    def diversified_rank(self, document_list, query_Q, K = 20):
+        self.query_vector_cache = {}
+
+    def diversified_rank(self, document_list, query_Q, k = 20):
+        print("Query length : " + str(len(query_Q)))
         pivot = []
         pivot_index = []
-        if query_Q not in self.vector_cache:
-            doc_vectors, identity2index, query_vec = calculate_vector_presentation(self.index, document_list, query_Q)
-            self.vector_cache[query_Q] = (doc_vectors, identity2index, query_vec)
-        else:
-            doc_vectors, identity2index, query_vec = self.vector_cache[query_Q]
+
+        doc_vectors = self.doc_vectors[[self.identity2index[doc_id] for doc_id in document_list]]
+
+        if query_Q not in self.query_vector_cache:
+            self.query_vector_cache[query_Q] = self.vectorizer.transform([query_Q])[0] # text --> sparse vector
+
+        query_vec = self.query_vector_cache[query_Q]
 
         similarity_matrix = self.sim2(doc_vectors, doc_vectors)
-        for i in range(K):
-
+        for i in range(k):
             all_values_min = 0 if i == 0 else np.array(all_values).min()
             # take 0 for first iteration, otherwise the minimum from last iteration
             all_values = []
-            for doc_ident in document_list:
+            for doc_vectors_index, doc_ident in enumerate(document_list):
 
                 if doc_ident not in pivot:
-                    #doc_u_text = self.index.getStrDoc(doc_ident)
-                    doc_u_vecs = doc_vectors[identity2index[doc_ident]]
-                    doc_index = identity2index[doc_ident]
-                    value = self.value_function(query_vec, doc_u_vecs, doc_index, pivot_index, similarity_matrix)
+                    doc_u_vecs = doc_vectors[doc_vectors_index]
+                    value = self.value_function(query_vec, doc_u_vecs, doc_vectors_index, pivot_index, similarity_matrix)
                 else:
                     value = all_values_min
                 all_values.append(value)
@@ -48,7 +53,20 @@ class GreedyRanking:
 
         return pivot
 
-def calculate_vector_presentation(index, document_list, query_Q):
+    def diversify(self, document_rank, query,
+                  by_top_n=100,
+                  order_n=5):
+
+        top_n_doc_ids = document_rank[:by_top_n, 0]
+
+        data = pd.DataFrame({'text': [self.index.getStrDoc(i) for i in top_n_doc_ids],
+                             'rank': np.arange(1, top_n_doc_ids.shape[0] + 1)},
+                            index=top_n_doc_ids)
+        diversified = self.diversified_rank(top_n_doc_ids, query, k=order_n)
+        return data.loc[diversified].reset_index()
+
+
+def calculate_vector_presentation(index, document_list):
     identity2index = {}
 
     docs = []
@@ -58,8 +76,7 @@ def calculate_vector_presentation(index, document_list, query_Q):
 
     vectorizer = CountVectorizer()
     doc_vectors = vectorizer.fit_transform(docs)
-    query_vec = vectorizer.transform([query_Q])[0]
-    return doc_vectors, identity2index, query_vec
+    return doc_vectors, identity2index, vectorizer
 
 def make_mmr(sim1, alpha = 0.5):
 

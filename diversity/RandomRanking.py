245e360aad05714afb0f52cd16952d438a6773dd
P4E <p4e@posteo.de>
2019-01-05 18:27:21 +0100

    random diversity

---
+++ diversity/RandomRanking.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class RandomDiversifier:
+    def __init__(self, index, seed):
+        numpy.random.seed(seed)
+
+    def diversify(self, document_rank, doc_limit=100, order_n=20):
+        top_docs = document_rank[:doc_limit]
+        np.random.shuffle(top_docs)
+        return top_docs
+        

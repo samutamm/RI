57c6f94f174df6aae70a1b97b0007cbdd92b2e1c
P4E <p4e@posteo.de>
2019-01-05 19:00:01 +0100

    coquille

--- diversity/RandomRanking.py
+++ diversity/RandomRanking.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 class RandomDiversifier:
-    def __init__(self, index, seed):
-        numpy.random.seed(seed)
+    def __init__(self, seed):
+        np.random.seed(seed)
 
     def diversify(self, document_rank, doc_limit=100, order_n=20):
         top_docs = document_rank[:doc_limit]
